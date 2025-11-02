b98f3f1102c396ef35d194c31b3f6dd170ed36a6
nlkguy <22cs097kris@ug.cusat.ac.in>
2025-11-02 23:06:11 +0530

    major remodel fellas, cleanup for old commented code

--- nofetch.sh
+++ nofetch.sh
@@ -4,7 +4,7 @@
 # author - nlkguy
 # version - 0.3
 
-VERSION = 0.3
+VERSION="0.3"
 
 usage(){
     cat <<EOF
@@ -22,8 +22,8 @@ EOF
 }
 
 version(){
-
-    echo "nofetch v$VERSION by nlkguy"
+    #printf "\n\tnofetch v$VERSION by nlkguy\n\n"
+    printf "\nnofetch v%s by nlkguy\n\n" "$VERSION"
 }
 
 logo(){
@@ -34,40 +34,20 @@ logo(){
 "
 }
 
-
-
 sysinfo(){
     
-
-    # printf "\tOS\t:\t" && . /etc/os-release && echo "$PRETTY_NAME" 
-
     . /etc/os-release 2>/dev/null
     os="${PRETTY_NAME:-Unknown OS}"
     kernel="$(uname -r)"
     printf "\tOS\t: $os ($kernel)"
 
-    #printf "\tHost\t:\t%s (%s)\n" "$(hostname)" "$(hostname -I | awk '{print $1}')"
-    #printf "\tKernel\t:\t" && uname -r
-
     host="$(hostname)"
     ip="$(ip addr show | awk '/inet / && !/127.0.0.1/ {sub(/\/.*/,"",$2); print $2; exit}')"
     printf "\n\tHost\t: $host (${ip:-no net})"
 
-
-
-    #printf "\tUptime\t:\t" 
-    #awk '{d=$1/86400; h=($1%86400)/3600; m=($1%3600)/60; printf "%dd %02dh %02dm\n", d,h,m}' /proc/uptime
-
-
-
-
-
-    #printf "\tCPU\t:\t" && awk -F: '/model name/ {print $2; exit}' /proc/cpuinfo | sed 's/^ //'
-
     cpu="$(awk -F: '/model name/{print $2; exit}' /proc/cpuinfo | sed 's/^ //')"
     arch="$(uname -m)"
     printf "\n\tCPU\t: $cpu ($arch)"
-
 
     gpu="$(lspci 2>/dev/null | awk -F': ' '/VGA|3D|Display/{print $3; exit}')"
     [ -z "$gpu" ] && gpu="$(grep -m1 'VGA' /proc/bus/pci/devices 2>/dev/null || echo 'N/A')"
@@ -75,17 +55,8 @@ sysinfo(){
     [ -z "$res" ] && res="$(xrandr 2>/dev/null | awk '/\*/ {print $1; exit}')"    
     printf "\n\tGPU\t: ${gpu:-N/A} (${res:-N/A})"
 
-
-
-
-    #printf "\tMemory\t:\t" 
-    #awk '/MemTotal/{t=$2} 
-    #    /MemAvailable/{a=$2}   
-    #    END{printf "%.1f / %.1f GiB\n", (t-a)/1048576, t/1048576}' /proc/meminfo
-
-    awk '/MemTotal/{t=$2}/MemAvailable/{a=$2}
-        END{u=(t-a); p=u*100/t; 
-        printf "\n\tMem\t: %.1f / %.1f GiB (%.0f%%)\n", u/1048576, t/1048576, p}' /proc/meminfo
+    awk '/MemTotal/{t=$2}/MemAvailable/{a=$2}END{u=(t-a); p=u*100/t; 
+    printf "\n\tMem\t: %.1f / %.1f GiB (%.0f%%)\n", u/1048576, t/1048576, p}' /proc/meminfo
 
     awk '{d=$1/86400; h=($1%86400)/3600; m=($1%3600)/60;
     printf "\tUptime\t: %dd %02dh %02dm\n", d,h,m}' /proc/uptime
