8ae988eee77424c4a28161356551bcfe53a36125
nlkguy <22cs097kris@ug.cusat.ac.in>
2025-11-02 21:30:39 +0530

    combined some metrics in same line

--- nofetch.sh
+++ nofetch.sh
@@ -9,23 +9,41 @@
 logo(){
     echo "
     ░█▀█░█▀█░█▀▀░█▀▀░▀█▀░█▀▀░█░█
-    ░█░█░█░█░█▀▀░█▀▀░░█░░█░░░█▀█ no-fetch ain't neofetch
+    ░█░█░█░█░█▀▀░█▀▀░░█░░█░░░█▀█ no-fetch ain't like neofetch
     ░▀░▀░▀▀▀░▀░░░▀▀▀░░▀░░▀▀▀░▀░▀
 "
 }
 main(){
     logo
 
-    printf "\tOS\t:\t" && . /etc/os-release && echo "$PRETTY_NAME" 
+    # printf "\tOS\t:\t" && . /etc/os-release && echo "$PRETTY_NAME" 
 
-    printf "\tHost\t:\t%s (%s)\n" "$(hostname)" "$(hostname -I | awk '{print $1}')"
+    . /etc/os-release 2>/dev/null
+    os="${PRETTY_NAME:-Unknown OS}"
+    kernel="$(uname -r)"
+    echo "\tOS\t: $os ($kernel)"
 
-    printf "\tKernel\t:\t" && uname -r
+    #printf "\tHost\t:\t%s (%s)\n" "$(hostname)" "$(hostname -I | awk '{print $1}')"
+    #printf "\tKernel\t:\t" && uname -r
 
-    printf "\tUptime\t:\t" 
-    awk '{d=$1/86400; h=($1%86400)/3600; m=($1%3600)/60; printf "%dd %02dh %02dm\n", d,h,m}' /proc/uptime
+    host="$(hostname)"
+    ip="$(ip addr show | awk '/inet / && !/127.0.0.1/ {sub(/\/.*/,"",$2); print $2; exit}')"
+    echo "\tHost\t: $host (${ip:-no net})"
 
-    printf "\tCPU\t:\t" && awk -F: '/model name/ {print $2; exit}' /proc/cpuinfo | sed 's/^ //'
+
+
+    #printf "\tUptime\t:\t" 
+    #awk '{d=$1/86400; h=($1%86400)/3600; m=($1%3600)/60; printf "%dd %02dh %02dm\n", d,h,m}' /proc/uptime
+
+
+
+
+
+    #printf "\tCPU\t:\t" && awk -F: '/model name/ {print $2; exit}' /proc/cpuinfo | sed 's/^ //'
+
+    cpu="$(awk -F: '/model name/{print $2; exit}' /proc/cpuinfo | sed 's/^ //')"
+    arch="$(uname -m)"
+    echo "\tCPU\t: $cpu ($arch)"
 
     printf "\tMemory\t:\t" 
     awk '/MemTotal/{t=$2} 
