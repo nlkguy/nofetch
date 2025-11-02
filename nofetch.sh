549fdf04c91f65e071ff6c2447eb196c820765ec
nlkguy <22cs097kris@ug.cusat.ac.in>
2025-11-02 21:48:03 +0530

    added uptime and memmory

--- nofetch.sh
+++ nofetch.sh
@@ -14,6 +14,7 @@ logo(){
 "
 }
 main(){
+    
     logo
 
     # printf "\tOS\t:\t" && . /etc/os-release && echo "$PRETTY_NAME" 
@@ -45,10 +46,28 @@ main(){
     arch="$(uname -m)"
     echo "\tCPU\t: $cpu ($arch)"
 
-    printf "\tMemory\t:\t" 
-    awk '/MemTotal/{t=$2} 
-        /MemAvailable/{a=$2}   
-        END{printf "%.1f / %.1f GiB\n", (t-a)/1048576, t/1048576}' /proc/meminfo
+
+    gpu="$(lspci 2>/dev/null | awk -F': ' '/VGA|3D|Display/{print $3; exit}')"
+    [ -z "$gpu" ] && gpu="$(grep -m1 'VGA' /proc/bus/pci/devices 2>/dev/null || echo 'N/A')"
+    res="$(awk -F'x' '/x/ {print $1"x"$2; exit}' /sys/class/graphics/fb0/modes 2>/dev/null)"
+    echo "\tGPU\t: ${gpu:-N/A} (${res:-N/A})"
+
+
+
+
+    #printf "\tMemory\t:\t" 
+    #awk '/MemTotal/{t=$2} 
+    #    /MemAvailable/{a=$2}   
+    #    END{printf "%.1f / %.1f GiB\n", (t-a)/1048576, t/1048576}' /proc/meminfo
+
+    awk '/MemTotal/{t=$2}/MemAvailable/{a=$2}
+        END{u=(t-a); p=u*100/t; 
+        printf "\tMem\t: %.1f / %.1f GiB (%.0f%%)\n", u/1048576, t/1048576, p}' /proc/meminfo
+
+    awk '{d=$1/86400; h=($1%86400)/3600; m=($1%3600)/60;
+    printf "\tUptime\t: %dd %02dh %02dm\n", d,h,m}' /proc/uptime
+
+        df -h / 2>/dev/null | awk 'NR==2 {printf "\tDisk\t: %s / %s (%s)\n", $3, $2, $6}'
 
 }
 
